c8e344a831d6034678e0ed8a278b2de22d768be6
Andrew Wang <wthsir@gmail.com>
2025-08-24 21:04:14 +1000

    refactor: add comment

--- FileService.Api/Dtos/CheckFileExistsResponseDto.cs
+++ FileService.Api/Dtos/CheckFileExistsResponseDto.cs
@@ -1,5 +1,8 @@
 ﻿namespace FileService.Api.Dtos
 {
+    /// <summary>
+    /// response dto for checking if a file exists
+    /// </summary>
     public record CheckFileExistsResponseDto
     {
         public bool IsExist { get; set; }
